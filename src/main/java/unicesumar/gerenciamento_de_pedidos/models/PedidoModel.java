110fc221bc5e95e70c1260204fc66e56fdc9e67d
Gustavo Pasquini <gustavopasquini2006@hotmail.com>
2026-03-22 18:53:25 -0300

    first commit

---
+++ src/main/java/unicesumar/gerenciamento_de_pedidos/models/PedidoModel.java
@@ -0,0 +1,54 @@
+package unicesumar.gerenciamento_de_pedidos.models;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="tb_pedido")
+public class PedidoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ped_id")
+    private Long id;
+
+    @Column(name="ped_data")
+    private LocalDateTime data;
+    private BigDecimal valorTotal;
+    private String status;
+
+    public PedidoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
